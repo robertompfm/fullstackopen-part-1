75890c1db81334c60050fa9a6695893a2e08bd3a
Roberto Mendes <robertompfm@gmail.com>
2024-05-07 21:18:21 -0300

    courseinfo

---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,44 @@
+const Header = ({title}) => <h1>{title}</h1>
+
+const Part = ({name, exercises}) => <p>{name} {exercises}</p>
+
+const Content = ({parts}) => (
+  <>
+    <Part name={parts[0].name} exercises={parts[0].exercises} />
+    <Part name={parts[1].name} exercises={parts[1].exercises} />
+    <Part name={parts[2].name} exercises={parts[2].exercises} />
+  </>
+)
+
+const Total = ({parts}) => <p>Number of exercises {parts.reduce((acc, curr) => acc + curr.exercises, 0)}</p>
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
